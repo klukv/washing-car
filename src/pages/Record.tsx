f8f4d0045d69759fccdbe8720d41d67b68047316
klukv <tema200260@gmail.com>
2023-04-19 22:15:35 +0300

    доделал проверку на выбор типа мойки

--- src/pages/Record.tsx
+++ src/pages/Record.tsx
@@ -3,11 +3,12 @@ import dayjs from "dayjs";
 import Calendar from "react-calendar";
 import TimePicker from "../components/TimePicker";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 import { getRecordsDate } from "../services/main";
 import { selectService } from "../redux/selectors";
 
 import { TMessageData } from "../utils/types";
-import { initialMessageData } from "../utils/const";
+import { catalogPage, initialMessageData } from "../utils/const";
 
 import "../scss/Record.scss";
 
@@ -15,11 +16,12 @@ const Record: React.FC = () => {
   const service = useSelector(selectService);
   const [selectDate, setSelectDate] = React.useState<string | null>(null);
   const [dateServices, setDateServices] = React.useState<string[] | null>(null);
-  const [dataMessage, setDataMessage] = React.useState<TMessageData>(initialMessageData);
+  const [dataMessage, setDataMessage] =
+    React.useState<TMessageData>(initialMessageData);
 
   const convertDate = (date: Date) => {
     const isSure = window.confirm("Вы уверены с выбором данной даты?");
-    if (isSure) {
+    if (isSure && service.nameService.length !== 0) {
       const newDate = dayjs(date)
         .format("YYYY-MM-DD")
         .split("-")
@@ -51,17 +53,26 @@ const Record: React.FC = () => {
   return (
     <main className="record">
       <div className="container">
-        <h1 className="record__title">{service.nameService}</h1>
+        {service.nameService.length === 0 ? (
+          <Link to={catalogPage} className="record__title-link">
+            Пожалуйста, выберите тип мойки
+          </Link>
+        ) : (
+          <h1 className="record__title">{service.nameService}</h1>
+        )}
+
         <div className="record__inner">
           <div className="record__calendar">
             <Calendar onClickDay={(value) => convertDate(value)} />
           </div>
-          {!dataMessage.isError && dateServices !== null ? (
+          {!dataMessage.isError &&
+          dateServices !== null &&
+          service.nameService.length !== 0 ? (
             <div className="record__date">
               <div className="record__date-inner">
                 <h2 className="record__date-title">Выберите время:</h2>
                 <div className="record__date-time">
-                  <TimePicker time={dateServices} date={selectDate}/>
+                  <TimePicker time={dateServices} date={selectDate} />
                 </div>
               </div>
             </div>
